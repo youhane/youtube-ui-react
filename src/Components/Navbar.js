2cdcc5ab788a070add8e1c9d56a530308b15c8bd
youhane <jason.sebastian154@gmail.com>
2021-08-09 11:54:48 +0700

    starting to know what to do

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,18 @@
 import react from 'react';
-import { Link } from 'react-router-dom';
 
 // Styles
 import '../index.css';
 
 // Assets
+import YoutubeLogo from "../Assets/youtube-logo.png";
 
 const Navbar = () => {
     return (
         <nav>
-
+            <img src={YoutubeLogo} alt='youtube-logo' className="w-24" />
+            <ul>
+                <input type='search' placeholder="Search" />
+            </ul>
         </nav>
     )
 };

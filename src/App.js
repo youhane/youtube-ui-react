8cfd9baa42d083c11552dfc070bf2e9bb8b23096
youhane <jason.sebastian154@gmail.com>
2021-08-09 13:38:27 +0700

    got fontawesome running!

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 
+
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare, faCoffee)
+
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,13 +5,18 @@ import '../index.css';
 
 // Assets
 import YoutubeLogo from "../Assets/youtube-logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
     return (
         <nav>
             <img src={YoutubeLogo} alt='youtube-logo' className="w-24" />
+            <div>
+                <input type='search' placeholder="Search" className="border-2 p-1 pl-4" size="40" />
+            </div>
             <ul>
-                <input type='search' placeholder="Search" />
+                <FontAwesomeIcon icon="check-square" />
+                Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
             </ul>
         </nav>
     )

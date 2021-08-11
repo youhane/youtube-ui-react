9465d8cdab58b79cda505e11c341cc6c9c21b61c
youhane <jason.sebastian154@gmail.com>
2021-08-11 13:08:37 +0700

    simplified some stuff

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -9,11 +9,10 @@ import { faClock, faList } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faClock, faList)
 
-const Video = ({ title, channel, views, duration, when }) => (
+const Video = ({ thumb, logo, title, channel, views, duration, when }) => (
     <div className="flex flex-col w-1/6 cursor-pointer">
         <div className="flex">
-            <img
-                src="https://i.ytimg.com/vi/diR-Mg4fE-0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCPcVMqtgUdlcps6G89UWMIc_WdZA" />
+            <img src={thumb} alt={thumb} />
             <p className="absolute bg-black text-white p-1 transform translate-x-53 translate-y-28 text-xs">{duration}</p>
             <div className="absolute text-white flex flex-col space-y-1 transform translate-x-56 translate-y-2">
                 <FontAwesomeIcon icon="clock" className="bg-black p-1 text-2xl" />
@@ -22,9 +21,7 @@ const Video = ({ title, channel, views, duration, when }) => (
         </div>
         <div className="flex mt-1">
             <div>
-                <img
-                    src="https://yt3.ggpht.com/ytc/AKedOLQCiv_FKTT6VBfuGXZF1FC7AUWk6S9cvRW0diw1=s68-c-k-c0x00ffffff-no-rj"
-                    className="w-3/5 rounded-full mx-auto mt-1" />
+                <img src={logo} className="w-3/5 rounded-full mx-auto mt-1" alt={logo} />
             </div>
             <div className="text-left">
                 <p className="font-semibold">{title}</p>
--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,6 +1,16 @@
 import react from "react";
 import Video from "./Video";
 
+// Assets
+import Thumbnail1 from '../Assets/Thumbnails/1.svg';
+import Thumbnail2 from '../Assets/Thumbnails/2.svg';
+import Thumbnail3 from '../Assets/Thumbnails/3.svg';
+import Thumbnail4 from '../Assets/Thumbnails/4.svg';
+import Thumbnail5 from '../Assets/Thumbnails/5.svg';
+import Thumbnail6 from '../Assets/Thumbnails/6.svg';
+import Thumbnail7 from '../Assets/Thumbnails/7.svg';
+import Thumbnail8 from '../Assets/Thumbnails/8.svg';
+
 const Videos = ({ title }) => (
     <div className="bg-gray-100 border-none">
         {title ? (
@@ -12,11 +22,41 @@ const Videos = ({ title }) => (
             null
         )}
         <div className="flex space-x-4 py-5 px-10">
-            <Video title="This is a title" channel="Channel name" views="412K" duration="1:08" when="2 years" />
-            <Video title="This is a title" channel="Channel name" views="412K" duration="1:08" when="2 years" />
-            <Video title="This is a title" channel="Channel name" views="412K" duration="1:08" when="2 years" />
-            <Video title="This is a title" channel="Channel name" views="412K" duration="1:08" when="2 years" />
-            <Video title="This is a title" channel="Channel name" views="412K" duration="1:08" when="2 years" />
+            <Video
+                thumb={Thumbnail1}
+                title="This is a title"
+                channel="Channel name"
+                views="412K"
+                duration="1:08"
+                when="2 years" />
+            <Video
+                thumb={Thumbnail2}
+                title="This is a title"
+                channel="Channel name"
+                views="412K"
+                duration="1:08"
+                when="2 years" />
+            <Video
+                thumb={Thumbnail3}
+                title="This is a title"
+                channel="Channel name"
+                views="412K"
+                duration="1:08"
+                when="2 years" />
+            <Video
+                thumb={Thumbnail4}
+                title="This is a title"
+                channel="Channel name"
+                views="412K"
+                duration="1:08"
+                when="2 years" />
+            <Video
+                thumb={Thumbnail5}
+                title="This is a title"
+                channel="Channel name"
+                views="412K"
+                duration="1:08"
+                when="2 years" />
         </div>
     </div>
 );

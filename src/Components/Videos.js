15e2bad763e7d33a5a359f7af412087726ea0cab
youhane <jason.sebastian154@gmail.com>
2021-09-01 12:58:50 +0700

    fixed some style

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -5,13 +5,11 @@ import Thumbnail1 from '../Assets/Thumbnails/1.svg';
 import Thumbnail2 from '../Assets/Thumbnails/2.svg';
 import Thumbnail3 from '../Assets/Thumbnails/3.svg';
 import Thumbnail4 from '../Assets/Thumbnails/4.svg';
-import Thumbnail5 from '../Assets/Thumbnails/5.svg';
 
 import Logo1 from '../Assets/Logo/1.svg';
 import Logo2 from '../Assets/Logo/2.svg';
 import Logo3 from '../Assets/Logo/3.svg';
 import Logo4 from '../Assets/Logo/4.svg';
-import Logo5 from '../Assets/Logo/5.svg';
 
 const Videos = ({ title }) => (
     <div className="bg-gray-100 border-none">
@@ -51,14 +49,6 @@ const Videos = ({ title }) => (
             <Video
                 thumb={Thumbnail4}
                 logo={Logo4}
-                title="This is a title"
-                channel="Channel name"
-                views="412K"
-                duration="1:08"
-                when="2 years" />
-            <Video
-                thumb={Thumbnail5}
-                logo={Logo5}
                 title="This is a title"
                 channel="Channel name"
                 views="412K"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
       spacing: {
         '53': '13.8rem',
         '25': '6.8rem',
+        '65': '17.2rem',
+        '64-5': '16.9rem',
       }
     }
   },
   variants: {
-    extend: {},
+    extend: { },
   },
   plugins: [],
 }
